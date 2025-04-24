ea04cb7ba9e3750bf25d758a4cec32174ed788a7
skillednoob <amol05jd@gmail.com>
2025-04-24 15:46:57 +0530

    Completed Scaffold context and create an Entity Data Model

---
+++ DBFirstCore.DataAccessLayer/Models/Category.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DBFirstCore.DataAccessLayer.Models;
+
+public partial class Category
+{
+    public byte CategoryId { get; set; }
+
+    public string CategoryName { get; set; } = null!;
+
+    public virtual ICollection<Product> Products { get; set; } = new List<Product>();
+}
