ab8b0a0d1a8078ac08aeb9463bc622219223a04f
skillednoob <amol05jd@gmail.com>
2025-04-25 15:11:20 +0530

    Completed READ operation

--- DBFirstCore.ConsoleApp/Program.cs
+++ DBFirstCore.ConsoleApp/Program.cs
@@ -1,10 +1,78 @@
-﻿namespace DBFirstCore.ConsoleApp
+﻿using DBFirstCore.DataAccessLayer.Models;
+using DBFirstCore.DataAccessLayer;
+
+namespace DBFirstCore.ConsoleApp
 {
-	internal class Program
+	public class Program
 	{
+		static QuickKartDbContext context;
+		static QuickKartRepository repository;
+
+		static Program()
+		{
+			context = new QuickKartDbContext();
+			repository = new QuickKartRepository(context);
+		}
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello, World!");
+			//var categories = repository.GetAllCategories();
+			//Console.WriteLine("----------------------------------");
+			//Console.WriteLine("CategoryId\tCategoryName");
+			//Console.WriteLine("----------------------------------");
+			//foreach (var category in categories)
+			//{
+			//	Console.WriteLine("{0}\t\t{1}", category.CategoryId, category.CategoryName);
+			//}
+
+
+			//byte categoryId = 2;
+			//List<Product> lstProducts = repository.GetProductsOnCategoryId(categoryId);
+			//if (lstProducts.Count == 0)
+			//{
+			//	Console.WriteLine("No products available under the category = " + categoryId);
+			//}
+			//else
+			//{
+			//	Console.WriteLine("{0,-15}{1,-30}{2,-15}{3,-10}{4}", "ProductId", "ProductName", "CategoryId", "Price", "QuantityAvailable");
+			//	Console.WriteLine("---------------------------------------------------------------------------------------");
+			//	foreach (var product in lstProducts)
+			//	{
+			//		Console.WriteLine("{0,-15}{1,-30}{2,-15}{3,-10}{4}", product.ProductId, product.ProductName, product.CategoryId, product.Price, product.QuantityAvailable);
+			//	}
+			//}
+
+			//byte categoryId = 1;
+			//Product product = repository.FilterProducts(categoryId);
+			//if (product == null)
+			//{
+			//	Console.WriteLine("No product details available");
+			//}
+			//else
+			//{
+			//	Console.WriteLine("{0,-15}{1,-30}{2,-15}{3,-10}{4}", "ProductId", "ProductName", "CategoryId", "Price", "QuantityAvailable");
+			//	Console.WriteLine("---------------------------------------------------------------------------------------");
+			//	Console.WriteLine("{0,-15}{1,-30}{2,-15}{3,-10}{4}", product.ProductId, product.ProductName, product.CategoryId, product.Price, product.QuantityAvailable);
+			//}
+			//Console.WriteLine();
+
+			string pattern = "BMW%"; //Only the products with the ProductName matching the pattern 'BMW%' are displayed.
+			List<Product> lstProducts = repository.FilterProductsUsingLike(pattern);
+			if (lstProducts.Count == 0)
+			{
+				Console.WriteLine("No products available with the = " + pattern);
+			}
+			else
+			{
+				Console.WriteLine("{0,-15}{1,-30}{2,-15}{3,-10}{4}", "ProductId", "ProductName", "CategoryId", "Price", "QuantityAvailable");
+				Console.WriteLine("---------------------------------------------------------------------------------------");
+				foreach (var product in lstProducts)
+				{
+					Console.WriteLine("{0,-15}{1,-30}{2,-15}{3,-10}{4}", product.ProductId, product.ProductName, product.CategoryId, product.Price, product.QuantityAvailable);
+				}
+			}
+			Console.WriteLine();
+
+
 		}
 	}
 }
---
+++ DBFirstCore.DataAccessLayer/QuickKartRepository.cs
@@ -0,0 +1,75 @@
+﻿using DBFirstCore.DataAccessLayer.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DBFirstCore.DataAccessLayer
+{
+	public class QuickKartRepository
+	{
+		private QuickKartDbContext context;
+		public QuickKartRepository(QuickKartDbContext context)
+		{
+			this.context = context;
+		}
+
+
+		public List<Category> GetAllCategories()
+		{
+			var categoriesList = context.Categories.OrderBy(c => c.CategoryId).ToList();
+			return categoriesList;
+		}
+
+		public List<Product> GetProductsOnCategoryId(byte categoryId)
+		{
+			List<Product> lstProducts = new List<Product>();
+			try
+			{
+				lstProducts = context.Products.Where(p => p.CategoryId == categoryId).ToList();
+			}
+			catch (Exception ex)
+			{
+				lstProducts = null;
+			}
+			return lstProducts;
+		}
+
+		public Product FilterProducts(byte categoryId)
+		{
+			Product product = new Product();
+			try
+			{
+				//product = context.Products.Where(p => p.CategoryId == categoryId).FirstOrDefault();
+				product = context.Products.Where(p => p.CategoryId == categoryId).OrderByDescending(p => p.Price).LastOrDefault();
+			}
+			catch (Exception ex)
+			{
+				product = null;
+			}
+			return product;
+		}
+
+		public List<Product> FilterProductsUsingLike(string pattern)
+		{
+			List<Product> lstProduct = new List<Product>();
+			try
+			{
+				lstProduct = context.Products.Where(p => EF.Functions.Like(p.ProductName, pattern)).ToList();
+			}
+			catch (Exception ex)
+			{
+				lstProduct = null;
+			}
+			return lstProduct;
+		}
+
+
+
+	}
+}
+
+//The context opens and closes connections as needed
+//the First(), selects the first element from the list of elements that satisfies the condition. If the list is empty, an exception is thrown
